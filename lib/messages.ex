ba7fd5ab77bf0be83009de8e13651acc1529dd59
Philip Szalwinski <pszalwinski@gmail.com>
2014-01-28 10:14:22 -0600

    WIP: Formatting board for display

--- lib/messages.ex
+++ lib/messages.ex
@@ -1,5 +1,20 @@
 defmodule Messages do
   def display_board(board) do
     rows = Board.rows(board)
+    Enum.map(rows, fn(row) -> format_row(row) end)
+  end
+
+  defp format_row(row) do
+    format_row(row, 1)
+  end
+
+  defp format_row([h|t], 1) do
+    square_one = " " <> to_string(first) <> " "
+    format_internal_square(square_one, t, 2)
+  end
+
+  defp format_internal_square(first, [h|t], acc) do
+    format_row(first <> "| " <> h <> " |",  acc + 1)
+    first
   end
 end
