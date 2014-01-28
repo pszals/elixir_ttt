046b70e97c24658ebd3e110975bc9dfad06f28dd
Philip Szalwinski <pszalwinski@gmail.com>
2014-01-28 16:34:34 -0600

    Adds messages

--- lib/messages.ex
+++ lib/messages.ex
@@ -1,2 +1,45 @@
 defmodule Messages do
+  def welcome do
+    "Welcome to Tic Tac Toe running on Elixir!"
+  end
+
+  def query_for_markers do
+    "Enter any letter of the English alphabet to serve as your marker"
+  end
+
+  def query_for_first_player do
+    "Which player should go first? (1 for player one, 2 for player 2)"
+  end
+
+  def announce_turn(piece) do
+    "It's #{piece}'s turn!"
+  end
+
+  def query_for_square do
+    "To place your piece, enter the number of an empty square on the board"
+  end
+
+  def error_not_empty do
+    "That square has already been taken. Try again"
+  end
+
+  def error_not_number do
+    "That is not a number. enter the number of an empty square on the board"
+  end
+
+  def game_over do
+    "Game Over! Or is it?"
+  end
+
+  def announce_winner(piece) do
+    "Player #{piece} wins!"
+  end
+
+  def announce_tie do
+    "Tie Game!"
+  end
+
+  def query_for_replay do
+    "Would you like to play again?"
+  end
 end
--- test/messages_test.exs
+++ test/messages_test.exs
@@ -1,37 +1,48 @@
-defmodule Messages do
+defmodule MessagesTest do
   use ExUnit.Case
   import Messages
 
   test "welcomes player to game" do
+    assert(welcome == "Welcome to Tic Tac Toe running on Elixir!")
   end
 
   test "asks for marker types" do
+    assert(query_for_markers == "Enter any letter of the English alphabet to serve as your marker") 
   end
 
   test "asks which marker goes first" do
+    assert(query_for_first_player == "Which player should go first? (1 for player one, 2 for player 2)")
   end
 
   test "announces whose turn it is" do
+    assert(announce_turn("K") == "It's K's turn!")
   end
 
   test "asks for input/square to mark" do
+    assert(query_for_square == "To place your piece, enter the number of an empty square on the board")
   end
 
   test "gives error message if square is not empty" do
+    assert(error_not_empty == "That square has already been taken. Try again")
   end
 
   test "gives error message if input is not a number" do
+    assert(error_not_number == "That is not a number. enter the number of an empty square on the board")
   end
 
   test "announces end of game" do
+    assert(game_over == "Game Over! Or is it?")
   end
 
   test "announces winner of game" do
+    assert(announce_winner("Q") == "Player Q wins!")
   end
 
   test "announces tie game" do
+    assert(announce_tie == "Tie Game!")
   end
 
   test "asks to replay or exit game" do
+    assert(query_for_replay == "Would you like to play again?")
   end
 end
