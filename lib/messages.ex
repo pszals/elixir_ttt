b9896b2dba5c1a465e95b19efee230a03c7c8f22
Philip Szalwinski <pszalwinski@gmail.com>
2014-01-28 10:17:52 -0600

    WIP: Formatting board

--- lib/messages.ex
+++ lib/messages.ex
@@ -4,17 +4,13 @@ defmodule Messages do
     Enum.map(rows, fn(row) -> format_row(row) end)
   end
 
-  defp format_row(row) do
-    format_row(row, 1)
-  end
-
-  defp format_row([h|t], 1) do
-    square_one = " " <> to_string(first) <> " "
-    format_internal_square(square_one, t, 2)
+  defp format_row([h|t]) do
+    square_one = " " <> to_string(h) <> " "
+    format_internal_square(square_one, t, 1)
   end
 
   defp format_internal_square(first, [h|t], acc) do
-    format_row(first <> "| " <> h <> " |",  acc + 1)
+    format_row(first <> "| " <> to_string(h) <> " |",  acc + 1)
     first
   end
 end
