7b9fde02886dfc6ec218425834caad10d08a4ad9
Philip Szalwinski <pszalwinski@gmail.com>
2014-01-29 13:53:42 -0600

    WIP: Configuring the game

---
+++ lib/configuration.ex
@@ -0,0 +1,20 @@
+defmodule Configuration do
+  import Messages
+
+  def configure_game(io) do
+    configurations = HashDict.new
+    width = configure_width(io)
+    HashDict.put(configurations, :width, width) 
+  end
+
+  defp configure_width(io) do
+    width = io.gets
+    display(io, query_for_board_width)
+    if Validations.valid_width?(width) do
+      width 
+    else
+      display(io, error_width)
+      configure_width(io)
+    end
+  end
+end
---
+++ test/configuration_test.exs
@@ -0,0 +1,9 @@
+defmodule ConfigurationTest do
+  use ExUnit.Case
+  import Configuration
+
+  test "gets configuration variables" do
+    configurations = configure_game(MockIO)
+    assert(HashDict.get(configurations, :width) == "3")
+  end
+end
