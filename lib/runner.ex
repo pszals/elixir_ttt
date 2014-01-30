b25862334710b1933f9eaad3b613c500fb1739ba
Philip Szalwinski <pszalwinski@gmail.com>
2014-01-30 15:23:10 -0600

    Can play a Human-Human game

--- lib/runner.ex
+++ lib/runner.ex
@@ -1,6 +1,35 @@
 defmodule Runner do
-  def play_game(io) do
-#   Configuration.configure_game(io)
-[1,2,3,4,5,6,7,8,9]
+  import HashDict, only: [get: 2]
+
+  def run(io) do
+    configurations = Configuration.configure_game(io)
+    play_game(io, configurations)
+  end
+
+  def play_game(io, configurations) do
+    board           = get(configurations, :board)
+    piece_one       = get(configurations, :piece_one)
+    piece_two       = get(configurations, :piece_two)
+    player_one_type = get(configurations, :player_one_type)
+    player_two_type = get(configurations, :player_two_type)
+
+    display_board(io, board)
+
+    piece_to_play   = GameRules.whose_turn(board, piece_one, piece_two)
+    new_board = Players.human_move(io, board, piece_to_play)
+    configurations = HashDict.put(configurations, :board, new_board)
+    if GameRules.game_over?(new_board) do
+      display_board(io, board)
+      GameRules.winning_piece(new_board) |>
+      Messages.announce_result |>
+      io.display
+    else
+      play_game(io, configurations)
+    end
+  end
+
+  defp display_board(io, board) do
+    BoardPresenter.format_board(board) |>
+    io.display
   end
 end
--- test/runner_test.exs
+++ test/runner_test.exs
@@ -2,7 +2,18 @@ defmodule RunnerTest do
   use ExUnit.Case
   import Runner
 
+  import HashDict, only: [put: 3]
+
   test "playing a game" do
-    assert(play_game(MockIo, configurations) == [1,2,3,4,5,6,7,8,9])
+    configurations = HashDict.new
+    configurations = put(configurations, :board, ["x", "x", 3,
+                                                  "o", "o", 6,
+                                                   7,   8,  9])
+    configurations = put(configurations, :piece_one, "x")
+    configurations = put(configurations, :piece_two, "o")
+    configurations = put(configurations, :player_one_type, Constants.human_player)
+    configurations = put(configurations, :player_two_type, Constants.human_player)
+
+    assert(play_game(MockIo, configurations) == "Player x wins!\nGame Over! Or is it?")
   end
 end
