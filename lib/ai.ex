0e6bbb9905cc01c33b5e09394033479e023c2b11
Philip Szalwinski <pszalwinski@gmail.com>
2014-01-31 15:01:00 -0600

    Working negamax, needs refactor to generalize for piece

--- lib/ai.ex
+++ lib/ai.ex
@@ -15,17 +15,37 @@ defmodule Ai do
     generate_next_level(piece, board, [], possible_moves)
   end
 
-  def minimax(board, maximizing, depth, piece_one, piece_two) do
-    if GameRules.game_over?(board) do
-      score_board(board, GameRules.whose_turn(board, piece_one, piece_two))
-    else
-      IO.puts GameRules.whose_turn(board, piece_one, piece_two)
-      generate_next_level(GameRules.whose_turn(board, piece_one, piece_two), board) |>
-      minimax(!maximizing, (depth + 1), piece_one, piece_two)
+  def negamax(board, maximizing, depth, piece_one, piece_two) do
+    cond do
+      GameRules.game_over?(board) ->
+        score_board(board, "o") / depth
+      maximizing ->
+        Enum.min(generate_next_level(GameRules.whose_turn(board, piece_one, piece_two), board) |>
+        Enum.map(fn(board) -> negamax(board, !maximizing, (depth + 1), piece_two, piece_one) end))
+      !maximizing ->
+        Enum.max(generate_next_level(GameRules.whose_turn(board, piece_one, piece_two), board) |>
+        Enum.map(fn(board) -> negamax(board, !maximizing, (depth + 1), piece_two, piece_one) end))
     end
   end
 
-  defp generate_next_level(piece, board, next_levels, []) do
+  def squares_with_scores(board, piece_to_play, other_piece) do
+    open_squares = Board.empty_squares(board)
+    next_level = generate_next_level(piece_to_play, board)
+    scores = Enum.map(next_level, fn(level) -> negamax(level, false, 1, other_piece, piece_to_play) end) 
+    Enum.zip(open_squares, scores)
+  end
+
+  def best_square(board, ai_piece, other_piece) do
+    squares_with_scores(board, ai_piece, other_piece) |>
+    max_value
+  end
+
+  def max_value(list_of_tuples) do
+    {square, _} = Enum.max_by(list_of_tuples, fn{_,v} -> v end)
+    square
+  end
+
+  defp generate_next_level(_, _, next_levels, []) do
     Enum.reverse(next_levels)
   end
 
--- test/ai_test.exs
+++ test/ai_test.exs
@@ -3,33 +3,78 @@ defmodule AiTest do
   import Ai
   
   test "scores a winning board" do
-    assert(score_board(["x", "x", "x", "o", "o", 6, 7, 8, 9], "x") == 1)
+    assert(score_board(["x","x","x", 
+                        "o","o", 6, 
+                         7,  8,  9], "x") == 1)
   end
 
   test "scores a losing board" do
-    assert(score_board(["x", "x", "x", "o", "o", 6, 7, 8, 9], "o") == -1)
+    assert(score_board(["x","x","x",
+                        "o","o", 6,
+                         7,  8,  9], "o") == -1)
   end
 
   test "scores a tie" do
     assert(score_board(["x","x","o",
-                       "o","o","x",
-                       "x","x","o"], "x") == 0)
+                        "o","o","x",
+                        "x","x","o"], "x") == 0)
   end
 
   test "generates next level of moves given a piece and board" do
-    assert(generate_next_level("o", ["x","x","o","o","o","x","x", 8,  9]) == 
-                       [["x","x","o","o","o","x","x","o",9], 
-                      ["x","x","o","o","o","x","x",8,"o"]]) 
-    assert(generate_next_level("x", ["x","x","o","o","o","x","x","o",9]) == [["x","x","o","o","o","x","x","o","x"]])
+    assert(generate_next_level("o", ["x","x","o",
+                                     "o","o","x",
+                                     "x", 8,  9]) == 
+                                                     [["x","x","o",
+                                                       "o","o","x",
+                                                       "x","o", 9], 
+                                                      ["x","x","o",
+                                                       "o","o","x",
+                                                       "x", 8, "o"]]) 
+    assert(generate_next_level("x", ["x","x","o",
+                                     "o","o","x",
+                                     "x","o", 9]) == [["x","x","o","o","o","x","x","o","x"]])
   end
 
-  test "minimax for a tie" do
-#   assert(minimax(["x","x","o",
-#                   "o","o","x",
-#                   "x", "x",  9], true, 1, "x", "o") == 0)
+  test "negamax for a tie" do
+    assert(negamax(["x","o","o",
+                    "o","o","x",
+                    "x","x", 9], true, 1, "o", "x") == 0)
   end
 
-# test "generates a list of maps with the square as the key and the score as the value" do
-#   assert(moves_with_scores(["x","x","o","o","o","x","x", 8,  9]) == [{8, 0}, {9, 0}])
-# end
+  test "negamax for a loss" do
+    assert(negamax(["x","o","o",
+                    "o","o","x",
+                    "x","x","x"], true, 1, "o", "x") == -1)
+  end
+
+  test "negamax for a win" do
+    assert(negamax(["o","o","o",
+                    "o","x","x",
+                    "x","o","o"], true, 1, "o", "x") == 1)
+  end
+
+  test "generates a list of maps with the square as the key and the score as the value" do
+    assert(squares_with_scores(["x","x","o",
+                                "o","o","x",
+                                "x","o", 9], "o", "x") == [{9, 0}])
+    assert(squares_with_scores([ 1, "x","o",
+                                "o","o","x",
+                                "x","o","x"], "o", "x") == [{1, 0}])
+    assert(squares_with_scores([ 1, "o","o",
+                                "o","x","x",
+                                "x","o","x"], "x", "o") == [{1, -1}])
+    assert(squares_with_scores([ 1,  2, "o",
+                                "o","o","x",
+                                "x","o","x"], "x", "o") == [{1, 0.5}, {2, 0}])
+  end
+
+  test "gets best square" do
+    assert(best_square([ 1,  2,  3,
+                        "o","o", 6,
+                        "x","x", 9], "o", "x") == 6)
+  end
+
+  test "gets key with max value" do
+    assert(max_value([{:a, 1}, {:b, 99}]) == :b)
+  end
 end
