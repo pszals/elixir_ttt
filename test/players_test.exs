e91b06228d2c75f9748cde6baae979189b1081c7
Philip Szalwinski <pszalwinski@gmail.com>
2014-02-05 13:39:17 -0600

    Uses constant computer player instead of magic number in test

--- test/players_test.exs
+++ test/players_test.exs
@@ -8,7 +8,7 @@ defmodule PlayersTest do
   test "computer makes a move" do
     assert(Players.make_move(MockIo, ["x","x",3,
                                       "o","o",6,
-                                       7,  8, 9], "x", "o", "2") == 
+                                       7,  8, 9], "x", "o", Constants.computer_player) == 
                                      ["x","x","x",
                                       "o","o",6,
                                        7,  8, 9])
